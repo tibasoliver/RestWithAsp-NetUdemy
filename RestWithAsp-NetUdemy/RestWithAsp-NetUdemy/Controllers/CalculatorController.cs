773ce320d97923952c7cf8fd52ed8a4d6ede0c11
Thiago Oliveira <33914941+tibasoliver@users.noreply.github.com>
2020-02-29 09:16:46 -0300

    .gitignore.fix

--- RestWithAsp-NetUdemy/RestWithAsp-NetUdemy/Controllers/CalculatorController.cs
+++ RestWithAsp-NetUdemy/RestWithAsp-NetUdemy/Controllers/CalculatorController.cs
@@ -12,7 +12,7 @@ namespace RestWithAsp_NetUdemy.Controllers
     {
 
         // GET api/values/5
-        [HttpGet("{firstNumber}/{secondNumber}")]
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public ActionResult Sum(string firstNumber, string secondNumber)
         {
             if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
@@ -23,6 +23,77 @@ namespace RestWithAsp_NetUdemy.Controllers
             return BadRequest("Invalid Input");
 
             
+        }
+
+        // GET api/values/5
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public ActionResult Subtraction(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var subtraction = ConvertToDecimal(firstNumber) - ConvertToDecimal(secondNumber);
+                return Ok(subtraction.ToString());
+            }
+            return BadRequest("Invalid Input");
+
+
+        }
+
+        // GET api/values/5
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public ActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var division = ConvertToDecimal(firstNumber) / ConvertToDecimal(secondNumber);
+                return Ok(division.ToString());
+            }
+            return BadRequest("Invalid Input");
+
+
+        }
+
+
+        // GET api/values/5
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public ActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var multiplication = ConvertToDecimal(firstNumber) * ConvertToDecimal(secondNumber);
+                return Ok(multiplication.ToString());
+            }
+            return BadRequest("Invalid Input");
+
+
+        }
+
+        // GET api/values/5
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public ActionResult Mean(string firstNumber, string secondNumber)
+        {
+            if (IsNumeric(firstNumber) && IsNumeric(secondNumber))
+            {
+                var mean = (ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber))/2;
+                return Ok(mean.ToString());
+            }
+            return BadRequest("Invalid Input");
+
+
+        }
+
+        // GET api/values/5
+        [HttpGet("square-roof/{number}")]
+        public ActionResult SquareRoot(string number)
+        {
+            if (IsNumeric(number))
+            {
+                var squareRoot = Math.Sqrt((double)ConvertToDecimal(number));
+                return Ok(squareRoot.ToString());
+            }
+            return BadRequest("Invalid Input");
+
+
         }
 
         private bool IsNumeric(string strNumber)
